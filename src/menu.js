7136ee3804eb4649bca98e6c88f58922db85de4d
DionataNunesGarcia <dionata.nunes.garcia@gmail.com>
2026-06-26 13:07:58 -0300

    fix: change propostas icon to currency-usd and ajuda icon to information-outline

--- src/menu.js
+++ src/menu.js
@@ -1,54 +1,54 @@
 export default [
-  'General',
+  'Geral',
   [
     { to: '/', icon: 'desktop-mac', label: 'Dashboard' }
   ],
-  'CRM',
+  'Licitações',
   [
-    { to: '/leads', label: 'Leads', icon: 'alert-circle' },
-    { to: '/projects', label: 'Projects', icon: 'briefcase' },
-    { to: '/proposals', label: 'Proposals', icon: 'at-sign' },
-    { to: '/invoices', label: 'Invoices', icon: 'file-document' },
-    { to: '/payments', label: 'Payments', icon: 'cash-multiple' }
+    { to: '/leads', label: 'Licitações', icon: 'alert-circle' },
+    { to: '/projects', label: 'Contratos', icon: 'briefcase' },
+    { to: '/proposals', label: 'Propostas', icon: 'currency-usd' },
+    { to: '/invoices', label: 'Notas Fiscais', icon: 'file-document' },
+    { to: '/payments', label: 'Pagamentos', icon: 'cash-multiple' }
   ],
-  'Reports',
+  'Relatórios',
   [
-    { to: '/reports/sales', label: 'Sales Report', icon: 'chart-line' },
-    { to: '/reports/leads', label: 'Leads Report', icon: 'alert-circle' },
-    { to: '/reports/projects', label: 'Project Report', icon: 'briefcase' },
-    { to: '/reports/timesheets', label: 'Timesheets', icon: 'clock' }
+    { to: '/reports/sales', label: 'Rel. Compras', icon: 'chart-line' },
+    { to: '/reports/leads', label: 'Rel. Licitações', icon: 'alert-circle' },
+    { to: '/reports/projects', label: 'Rel. Contratos', icon: 'briefcase' },
+    { to: '/reports/timesheets', label: 'Rel. Horas', icon: 'clock' }
   ],
-  'Apps',
+  'Aplicativos',
   [
     { to: '/apps/chat', label: 'Chat', icon: 'chat' },
-    { to: '/apps/email', label: 'Email', icon: 'email' },
-    { to: '/apps/tasks', label: 'Tasks', icon: 'checkbox-marked' },
-    { to: '/apps/notes', label: 'Notes', icon: 'note-text' },
-    { to: '/apps/calendar', label: 'Calendar', icon: 'calendar-month' }
+    { to: '/apps/email', label: 'E-mail', icon: 'email' },
+    { to: '/apps/tasks', label: 'Tarefas', icon: 'checkbox-marked' },
+    { to: '/apps/notes', label: 'Notas', icon: 'note-text' },
+    { to: '/apps/calendar', label: 'Calendário', icon: 'calendar-month' }
   ],
-  'Examples',
+  'Ferramentas',
   [
-    { to: '/charts', label: 'Charts', icon: 'chart-bar' },
-    { to: '/tables', label: 'Tables', icon: 'table', updateMark: true },
-    { to: '/forms', label: 'Forms', icon: 'square-edit-outline' },
-    { to: '/profile', label: 'Profile', icon: 'account-circle' },
-    { to: '/settings', label: 'Settings', icon: 'settings' },
-    { to: '/help', label: 'Help Center', icon: 'life-buoy' },
+    { to: '/charts', label: 'Gráficos', icon: 'chart-bar' },
+    { to: '/tables', label: 'Tabelas', icon: 'table', updateMark: true },
+    { to: '/forms', label: 'Formulários', icon: 'square-edit-outline' },
+    { to: '/profile', label: 'Perfil', icon: 'account-circle' },
+    { to: '/settings', label: 'Configurações', icon: 'settings' },
+    { to: '/help', label: 'Ajuda', icon: 'information-outline' },
     {
-      label: 'Auth Pages',
-      subLabel: 'Login, Register, etc.',
+      label: 'Autenticação',
+      subLabel: 'Login, Registrar, etc.',
       icon: 'lock',
       menu: [
         { to: '/login', label: 'Login' },
-        { to: '/register', label: 'Register' },
-        { to: '/reset-password', label: 'Reset Password' },
-        { to: '/verify-otp', label: 'Verify OTP' },
-        { to: '/404', label: '404 Error' },
-        { to: '/maintenance', label: 'Maintenance' }
+        { to: '/register', label: 'Cadastrar' },
+        { to: '/reset-password', label: 'Redefinir Senha' },
+        { to: '/verify-otp', label: 'Verificar OTP' },
+        { to: '/404', label: 'Erro 404' },
+        { to: '/maintenance', label: 'Manutenção' }
       ]
     }
   ],
-  'About',
+  'Sobre',
   [
     { href: 'https://justboil.me/bulma-admin-template/one/', label: 'Premium', icon: 'monitor', target: '_blank' },
     { href: 'https://github.com/vikdiesel/admin-one-vue-bulma-dashboard', label: 'GitHub', icon: 'github-circle', target: '_blank' }
